43fde9c099f3bfeed86273867fda6db544bace0b
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-10-18 00:14:46 -0600

    Created forms for examen ordinario

---
+++ EPV_1970052/EPV_1970052/Form10.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form10
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lblNumero = New System.Windows.Forms.Label()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.lblText = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(435, 180)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 35)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Menu"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lblNumero
+        '
+        Me.lblNumero.AutoSize = True
+        Me.lblNumero.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumero.Location = New System.Drawing.Point(149, 135)
+        Me.lblNumero.Name = "lblNumero"
+        Me.lblNumero.Size = New System.Drawing.Size(143, 20)
+        Me.lblNumero.TabIndex = 12
+        Me.lblNumero.Text = "Ingresa un numero"
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Location = New System.Drawing.Point(298, 137)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(196, 20)
+        Me.txtNumero.TabIndex = 11
+        '
+        'lblText
+        '
+        Me.lblText.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblText.Location = New System.Drawing.Point(52, 34)
+        Me.lblText.Name = "lblText"
+        Me.lblText.Size = New System.Drawing.Size(510, 100)
+        Me.lblText.TabIndex = 10
+        Me.lblText.Text = "Crea un programa que solicite un correo electronico y valide si esta correctament" &
+    "e formateado, es decir, que contenga un simbolo '@' y un dominio (por ejemplo ""." &
+    "com"")"
+        '
+        'Form10
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 246)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblNumero)
+        Me.Controls.Add(Me.txtNumero)
+        Me.Controls.Add(Me.lblText)
+        Me.Name = "Form10"
+        Me.Text = "Form10"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblNumero As Label
+    Friend WithEvents txtNumero As TextBox
+    Friend WithEvents lblText As Label
+End Class
